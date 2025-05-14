51180a225a188b1b84f9c1b68d376c79e511fed1
Nidhipal09 <nidhipalmms@gmail.com>
2025-05-14 13:17:05 +0530

    Improved: moved cors config to a different class

--- src/main/java/com/employeeportal/config/SecurityConfig.java
+++ src/main/java/com/employeeportal/config/SecurityConfig.java
@@ -64,20 +64,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-
 }
 
 
