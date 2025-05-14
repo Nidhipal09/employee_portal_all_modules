1a80ac2daccdcc87b739093080ca09f7102e31d6
Nidhipal09 <nidhipalmms@gmail.com>
2025-05-14 11:51:37 +0530

    Fixed: typo

--- src/main/java/com/employeeportal/dto/onboarding/UpdateStatusRequest.java
+++ src/main/java/com/employeeportal/dto/onboarding/UpdateStatusRequest.java
@@ -2,9 +2,11 @@ package com.employeeportal.dto.onboarding;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateStatusRequest {
     private String status;
 
